9953d443e9da8960fb4dd4b074f060591b086a73
Jason Zheng <jason1996zheng@gmail.com>
2021-09-04 09:49:14 -0400

    Create minDistance.py
    created prompt

---
+++ Interviews/AWS/minDistance.py
@@ -0,0 +1,20 @@
+'''
+Write an algorithm to determine the minimum distance required for the truck to deliver the order.
+
+Delivery area represented as a two-dimensional grid of integers, where each integer represents one cell. 
+
+1 - Accessible areas
+0 - Areas' without road
+9 - Destination
+
+[[1 0 0]
+ [1 0 0]
+ [1 9 0]]
+
+Input: Area (2D grid of integers)
+Output: Return integer representing the total diistance traveled or return -1.
+'''
+
+def minDistance(area):
+  
+  return -1
