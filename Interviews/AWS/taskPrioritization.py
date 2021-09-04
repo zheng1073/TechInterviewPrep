01f891e5c3362bc55406fa68068fdf440e0a23b0
Jason Zheng <jason1996zheng@gmail.com>
2021-09-04 09:39:23 -0400

    Create taskPrioritization.py
    created prompt

---
+++ Interviews/AWS/taskPrioritization.py
@@ -0,0 +1,7 @@
+###
+Sort a list of all orders in the order queue to assist in prioritization of fullfilment. They should be sorted in the following order:
+
+1. Prime orders should be returned first (metadata only lowercase letters/sorted lexicographically) then non-Prime.  
+2. In case there is a tie, the identifier should be used.
+3. The remaining non-Prime order will remain in the order that they came in.
+###
