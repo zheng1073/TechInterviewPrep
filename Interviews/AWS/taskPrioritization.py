1cc0184cd68a513fd872049f47e7d78cad4033a3
Jason Zheng <jason1996zheng@gmail.com>
2021-09-04 09:50:57 -0400

    Update taskPrioritization.py

--- Interviews/AWS/taskPrioritization.py
+++ Interviews/AWS/taskPrioritization.py
@@ -1,7 +1,13 @@
-###
+'''
 Sort a list of all orders in the order queue to assist in prioritization of fullfilment. They should be sorted in the following order:
 
 1. Prime orders should be returned first (metadata only lowercase letters/sorted lexicographically) then non-Prime.  
 2. In case there is a tie, the identifier should be used.
 3. The remaining non-Prime order will remain in the order that they came in.
-###
+
+Input: orderList (list of strings representing all orders) 
+Output: list of orders
+'''
+
+def taskPrioritization(orderList):
+  
